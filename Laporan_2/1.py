848ce5df5fec8ecc8492e3e2f4b212be17b36f36
TimotiusEkanaT <117902158+TimotiusEkanaT@users.noreply.github.com>
2025-02-16 21:45:57 +0700

    Delete Laporan_2 directory

--- Laporan_2/1.py
+++
@@ -1,6 +0,0 @@
-berat = float(input("Masukkan berat badan (kg) : "))
-tinggi = float(input("Masukkan tinggi badan (m) : "))
-
-BMI = berat/(tinggi**2)
-
-print("BMI = ", BMI)
--- Laporan_2/2.py
+++
@@ -1,4 +0,0 @@
-x = int(input("Masukkan x : "))
-funcx = ((2*(x**3))+(2*x)+(15/x))
-
-print("f(x) = ", funcx)
